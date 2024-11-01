0334ad37c88f4c7fb639f4c76b2418430b1ed631
Bruno <brunopirizoliveira@gmail.com>
2024-10-31 21:06:15 -0300

    First commit

---
+++ components/HLSPlayer.tsx
@@ -0,0 +1,46 @@
+// components/HLSPlayer.tsx
+import { useEffect, useRef } from 'react';
+import Hls from 'hls.js';
+
+interface HLSPlayerProps extends React.VideoHTMLAttributes<HTMLVideoElement> {
+  src: string;
+}
+
+const HLSPlayer: React.FC<HLSPlayerProps> = ({ src, ...props }) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      const video = videoRef.current;
+
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(src);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+          video.play();
+        });
+
+        return () => {
+          hls.destroy();
+        };
+      } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = src;
+        video.addEventListener('loadedmetadata', () => {
+          video.play();
+        });
+      }
+    }
+  }, [src]);
+
+  return (
+    <video
+      ref={videoRef}
+      controls
+      style={{ width: '100%', height: 'auto' }}
+      {...props}
+    />
+  );
+};
+
+export default HLSPlayer;
---
+++ pages/types/hls.d.ts
@@ -0,0 +1,6 @@
+// types/hls.d.ts
+declare module 'hls.js' {
+    import Hls from 'hls.js';
+    export default Hls;
+  }
+  
